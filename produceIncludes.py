3efff50a0e87ab16e6cdfafefda589268503254f
Onur <onura@Onurs-MacBook-Pro.local>
2018-12-13 22:37:17 +0300

    added scanning shader files with .glsl extension, replace include statements with respective modules and creating new shader files in the output folder

--- produceIncludes.py
+++ produceIncludes.py
@@ -1,11 +1,12 @@
 import os
+import re
+import pathlib
 
-
-def load_modules(module_files):
+def load_modules(module_files, dir):
     moduleMap = {}
     for file in module_files:
         name = file[:file.index(".glsl")]
-        f = open("./util/" + file, "r")
+        f = open(os.path.normpath(dir + "/" + file), "r")
         moduleMap[name] = f.read()
         f.close()
 
@@ -13,5 +14,28 @@ def load_modules(module_files):
 
 
 if __name__ == "__main__":
-    moduleFileList = os.listdir("./util")
-    moduleMap = loadModules(moduleFileList)
+    baseDirectory = "."
+    baseModuleDirectory = "./util"
+    moduleFileList = os.listdir(baseModuleDirectory)
+    moduleMap = load_modules(moduleFileList, baseModuleDirectory)
+
+    outputFile = "output"
+    pathlib.Path(outputFile).mkdir(exist_ok=True)
+
+    fileList = os.listdir(baseDirectory)
+    shaderList = [f for f in fileList if f.endswith(".glsl")]
+    regex_word="\#include\s+(.*)$"
+    for shader in shaderList:
+        with open(shader) as fh:
+            newFile = open(outputFile + "/" + shader, "w")
+            for line in fh:
+                toWrite = line
+                if line.startswith("#include"):
+                    m = re.search(regex_word, line)
+                    module = m.group(1)
+                    line = moduleMap[module]
+                    print(m.group(1), moduleMap[module])
+                newFile.write(line)
+            newFile.close()
+
+    print(shaderList)
